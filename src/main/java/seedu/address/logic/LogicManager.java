b597c7050cce080cd909e0ef1a8a777e9e6bc82a
Elijah Ku <elijahku716@gmail.com>
2023-03-18 18:36:18 +0800

    Start conversion of test cases to new format
    With the implementation of new classes replacing the old Addressbook
    ones such as Person, the names of specific methods and the tests also
    need to be adjusted accordingly to fit with the new context.
    
    Let's begin converting the names over to the proper conventions.

--- src/main/java/seedu/address/logic/LogicManager.java
+++ src/main/java/seedu/address/logic/LogicManager.java
@@ -61,7 +61,7 @@ public class LogicManager implements Logic {
 
     @Override
     public ObservableList<Entity> getFilteredPersonList() {
-        return model.getFilteredPersonList();
+        return model.getFilteredEntityList();
     }
 
     @Override
